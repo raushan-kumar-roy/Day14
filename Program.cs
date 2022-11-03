abb254a060f388d0cbe26e1ef94e84bda4634631
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-11-03 11:36:41 +0530

    queue

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Collections;
+namespace queue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Queue queue = new Queue();  
+
+            queue.Enqueue(1);
+            queue.Enqueue("my_queue");
+            
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
