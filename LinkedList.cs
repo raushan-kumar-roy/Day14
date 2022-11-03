7b89b1c73f794935cfddef2b6ff682a6e82e0f54
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-11-03 11:44:17 +0530

    linkedList

---
+++ LinkedList.cs
@@ -0,0 +1,28 @@
+﻿
+namespace linkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+
+
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.EnQueue(56);
+            linkedListQueue.EnQueue(30);
+            linkedListQueue.EnQueue(70);
+
+            linkedListQueue.Dequeue(56);
+            linkedListQueue.Dequeue(30);
+            linkedListQueue.Dequeue(70);
+            linkedListQueue.Display();
+        }
+    }
+}
